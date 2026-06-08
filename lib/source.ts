57e85262082b84853315e1666ab050b4a3e736e1
Ian Ngethe Muchiri <100555904+ianmuchyri@users.noreply.github.com>
2026-06-08 11:31:01 +0300

    NOISSUE - Fix type check (#131)
    * fix type check
    
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>
    
    * fix lint
    
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>
    
    ---------
    
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- lib/source.ts
+++ lib/source.ts
@@ -1,8 +1,16 @@
 import { docs } from "fumadocs-mdx:collections/server";
-import { loader } from "fumadocs-core/source";
+import { type InferPageType, loader } from "fumadocs-core/source";
 
 export const source = loader({
   baseUrl: "/",
   source: docs.toFumadocsSource(),
   plugins: [],
 });
+
+export async function getLLMText(page: InferPageType<typeof source>) {
+  const processed = await page.data.getText("processed");
+
+  return `# ${page.data.title}
+
+${processed}`;
+}
